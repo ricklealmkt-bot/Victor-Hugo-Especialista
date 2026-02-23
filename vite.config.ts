bdfc6006ccc45aba94108c35d5843eabc0e5b9af
Ricardo Leal <ricklealmkt@gmail.com>
2026-02-23 15:26:31 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// Forçamos o retorno de um objeto claro para o compilador
 export default defineConfig(() => {
   return {
     plugins: [react()],
